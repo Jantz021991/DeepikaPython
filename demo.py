15f5e9f08fd42a05c270abc529fae8e8aab409ef
Deepika Angelene Jantz <djantz@unomaha.edu>
2017-06-18 23:52:08 -0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 import paho.mqtt.client as mqtt # mosquitto.py is deprecated
 import time
 
-mqttc = mqtt.Connect("ioana")
+mqttc = mqtt.Client("ioana")
 mqttc.connect("192.168.2.5", 1883, 60)
 #mqttc.subscribe("test/", 2) # <- pointless unless you include a subscribe callback
 mqttc.loop_start()
