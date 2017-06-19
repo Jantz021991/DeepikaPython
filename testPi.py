50f36712a4165f60c31d837473ea27be4a58292b
Deepika Angelene Jantz <djantz@unomaha.edu>
2017-06-18 21:20:56 -0500

    Update testPi.py

--- testPi.py
+++ testPi.py
@@ -1,7 +1,7 @@
 import paho.mqtt.publish as publish
 import time
 print("0")
-publish.single("ledStatus", "0", hostname="192.168.2.5")
+publish.single("ledStatus", "0", hostname="DESKTOP-FI57R0D")
 time.sleep(1)
 print("1")
-publish.single("ledStatus", "1", hostname="192.168.2.5")
+publish.single("ledStatus", "1", hostname="DESKTOP-FI57R0D")
